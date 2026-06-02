f55a3638629a37e36e1f3ca2a4a0d80d5eefd0fb
krishjuit <krisha.it.ug@jadavpuruniversity.in>
2026-06-02 20:16:55 +0530

    fix cloudinary import config bug

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
@@ -11,8 +13,6 @@ import couponRouter from "./routes/couponRoute.js";
 import reviewRouter from "./routes/reviewRoute.js";
 import analyticsRouter from "./routes/analyticsRoute.js";
 import notificationRouter from "./routes/notificationRoute.js";
-
-dotenv.config();
 
 const app = express(); // ✅ MUST be first
 
